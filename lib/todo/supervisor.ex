04cc2da72d64ce2f8b0196614d563723a6bf444a
Arraying <paul.huebner@googlemail.com>
2023-07-12 12:05:50 +0200

    Restart all if process registry fails.

--- lib/todo/supervisor.ex
+++ lib/todo/supervisor.ex
@@ -2,7 +2,7 @@ defmodule Todo.Supervisor do
   def start_link do
     Supervisor.start_link(
       [Todo.ProcessRegistry, Todo.Database, Todo.Cache],
-      strategy: :one_for_one
+      strategy: :rest_for_one
     )
   end
 end
